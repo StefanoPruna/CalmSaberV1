e1b1c597bf4cf2b2a22d939ce4a6a43e87438ec9
StefanoPruna <stefano.pruna@googlemail.com>
2024-03-05 19:56:14 +1100

    Added the movement and Sabers

---
+++ Assets/Scripts/Saber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        //Movement of the Saber to 130 degrees to cut the cube and destroy it when hit it
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        {
+            //Here is when the Saber is from up of 130 to down towards the cube 
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+                Destroy(hit.transform.gameObject);
+        }
+
+        previousPos = transform.position;
+    }
+}
