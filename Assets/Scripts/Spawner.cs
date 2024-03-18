b82f35a5806c336e55b1859bdadfdd733d2d73fb
Christess12 <supercoder12@gmail.com>
2024-03-18 11:11:10 +1100

    CD - 18/3/24
    - Cube spawning locations should vary horizontally now

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,7 +7,12 @@ public class Spawner : MonoBehaviour
     public GameObject[] cubes;
     public Transform[] spawnPoints;
     public float beat;
+    private float horizontalVariation = 10.0f;
+
     private float timer;
+    private int spawnIndex = 0; // Spawn location number
+
+    public global::System.Single HorizontalVariation { get => horizontalVariation; set => horizontalVariation = value; }
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +25,10 @@ public class Spawner : MonoBehaviour
     {
         if(timer>beat)
         {
+            float horizontalOffset = Random.Range(-HorizontalVariation, HorizontalVariation) * 10;
+
             GameObject cube = Instantiate(cubes[Random.Range(0, 2)], spawnPoints[Random.Range(0, 4)]);
-            cube.transform.localPosition = Vector3.zero;
+            cube.transform.localPosition = Vector3.zero + Vector3(horizontalOffset, 0f, 0f);
             cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
             timer -= beat;
         }
