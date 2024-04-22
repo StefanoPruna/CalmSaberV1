0424f4cb091bc99edafd820361edd5769e0926e8
Christess12 <supercoder12@gmail.com>
2024-04-22 15:15:39 +1000

    Final Spawner changes
    - Spawner timing moved to this branch

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -20,10 +20,10 @@ public class Spawner : MonoBehaviour
     void Start()
     {
 
-        beat = 2;
-        beatStage = 1;
-        StageChangeTime = 5;
-
+        beat = 2.7f;
+        beatStage = 0;
+        StageChangeTime = 18f;
+        nextStageBeatRate = 2.2f;
 
     }
 
@@ -34,8 +34,111 @@ public class Spawner : MonoBehaviour
         {
 
             beat = nextStageBeatRate;
-            beatStage = beatStage +1;
+            beatStage = beatStage + 1;
 
+            if (beatStage == 1)
+            {
+
+                nextStageBeatRate = 2.2f;
+                StageChangeTime = 41f;
+
+            }
+
+            if (beatStage == 2)
+            {
+
+                nextStageBeatRate = 2f;
+                StageChangeTime = 65f;
+
+            }
+
+            if (beatStage == 3)
+            {
+
+                nextStageBeatRate = 2.2f;
+                StageChangeTime = 89f;
+
+            }
+
+            if (beatStage == 4)
+            {
+
+                nextStageBeatRate = 2f;
+                StageChangeTime = 113f;
+
+            }
+
+            if (beatStage == 5)
+            {
+
+                nextStageBeatRate = 2.7f;
+                StageChangeTime = 136f;
+
+            }
+
+            if (beatStage == 6)
+            {
+
+                nextStageBeatRate = 2f;
+                StageChangeTime = 160f;
+
+            }
+
+            if (beatStage == 7)
+            {
+
+                nextStageBeatRate = 2.2f;
+                StageChangeTime = 183f;
+
+            }
+
+            if (beatStage == 8)
+            {
+
+                nextStageBeatRate = 2f;
+                StageChangeTime = 207f;
+
+            }
+
+            if (beatStage == 9)
+            {
+
+                nextStageBeatRate = 2f;
+                StageChangeTime = 229f;
+
+            }
+
+            if (beatStage == 10)
+            {
+
+                nextStageBeatRate = 1.8f;
+                StageChangeTime = 255f;
+
+            }
+
+            if (beatStage == 11)
+            {
+
+                nextStageBeatRate = 1.8f;
+                StageChangeTime = 279f;
+
+            }
+
+            if (beatStage == 12)
+            {
+
+                nextStageBeatRate = 2.7f;
+                StageChangeTime = 289f;
+
+            }
+
+            if (beatStage == 13)
+            {
+
+                nextStageBeatRate = 30f;
+                StageChangeTime = 295f;
+
+            }
 
         }
 
@@ -45,37 +148,15 @@ public class Spawner : MonoBehaviour
             {
 
                 GameObject cube = Instantiate(cubes[Random.Range(0, 2)], spawnPoints[Random.Range(0, 4)]);
-                //cube.transform.localPosition = Vector3.zero + new Vector3(horizontalOffset, 0f, 0f);
+                //cube.transform.localPosition = Vector3.zero + new Vector3(horizontalOffset, 0.5f, 2f);
+                cube.transform.localPosition = Vector3.zero + new Vector3(Random.Range(-1, 1f), Random.Range(0f, 1f));
                 cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
                 timer -= beat;
 
             }
-            
-        }
-        
-        if (beatStage == 1)
-        {
-
-            nextStageBeatRate = 1;
-            StageChangeTime = 15f;
 
         }
 
-        if (beatStage == 2)
-        {
-
-            nextStageBeatRate = 5f;
-            StageChangeTime = 30f;
-
-        }
-
-        if (beatStage == 3)
-        {
-
-            nextStageBeatRate = 0.5f;
-            StageChangeTime = 45;
-
-        }
 
         timer += Time.deltaTime;
 
